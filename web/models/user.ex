137d32eaf8f937e3580730573541b4cc09bae772
Chad Ostrowski <hi@chadoh.com>
2018-05-06 10:02:55 -0400

    Fix indentation
    Most of the indentation sloppiness was in the templates here.

--- web/models/user.ex
+++ web/models/user.ex
@@ -17,18 +17,18 @@ defmodule Shblog.User do
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
-      struct
-      |> cast(params, [:username, :email, :password, :password_confirmation])
-      |> validate_required([:username, :email, :password, :password_confirmation])
-      |> hash_password
-    end
+    struct
+    |> cast(params, [:username, :email, :password, :password_confirmation])
+    |> validate_required([:username, :email, :password, :password_confirmation])
+    |> hash_password
+  end
 
-    defp hash_password(changeset) do
-      if password = get_change(changeset, :password) do
-        changeset
-        |> put_change(:password_digest, hashpwsalt(password))
-      else
-        changeset
-      end
+  defp hash_password(changeset) do
+    if password = get_change(changeset, :password) do
+      changeset
+      |> put_change(:password_digest, hashpwsalt(password))
+    else
+      changeset
     end
+  end
 end
